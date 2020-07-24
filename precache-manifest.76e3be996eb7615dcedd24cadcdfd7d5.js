627ab24613d0349ec0230aabf6e8bf7c31ec0b29
venky6849 <55758190+venky6849@users.noreply.github.com>
2020-07-24 23:14:40 +0530

    Updates

---
+++ precache-manifest.76e3be996eb7615dcedd24cadcdfd7d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80938de333c81c999f57947fc417b1c4",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "9d35d8d2eb9324cf2711",
+    "url": "/coronatracker/static/css/main.9f87b55d.chunk.css"
+  },
+  {
+    "revision": "318b7383a66c47617805",
+    "url": "/coronatracker/static/js/2.66075741.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronatracker/static/js/2.66075741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d35d8d2eb9324cf2711",
+    "url": "/coronatracker/static/js/main.52afb5cd.chunk.js"
+  },
+  {
+    "revision": "d08c72dc70dbac9e3569",
+    "url": "/coronatracker/static/js/runtime-main.b4cd5e5e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronatracker/static/media/image.d7265326.png"
+  }
+]);
